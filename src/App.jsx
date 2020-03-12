7a4f472d1b28617e9ceffa7f333dd3374c60841d
Kyokatarz <51933605+Kyokatarz@users.noreply.github.com>
2020-03-12 20:43:59 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    
+    render(){
+        return(
+        <div> HELLO WORLD</div>
+        )
+    }
+}
+    
+    ReactDOM.render(<App/>, document.querySelector('#App'))
