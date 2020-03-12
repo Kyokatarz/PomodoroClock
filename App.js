7b8dce11d6a181421d85b9011d1a0d995263ced6
Kyokatarz <51933605+Kyokatarz@users.noreply.github.com>
2020-03-12 23:42:46 +0200

    new branch

--- App.js
+++ App.js
@@ -12,16 +12,173 @@ var App = function (_React$Component) {
     function App(props) {
         _classCallCheck(this, App);
 
-        return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+        var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+
+        _this.state = {
+            running: false,
+            sessionMin: 25,
+            breakMin: 5,
+            minuteLeft: 25,
+            secondLeft: 0,
+            clockType: 'Session'
+        };
+        _this.countSession = _this.countSession.bind(_this);
+        _this.countBreak = _this.countBreak.bind(_this);
+        return _this;
     }
 
     _createClass(App, [{
+        key: 'sessionDecrease',
+        value: function sessionDecrease() {
+            if (!this.state.running) {
+                if (this.state.sessionMin > 0) {
+                    this.setState(function (_ref) {
+                        var sessionMin = _ref.sessionMin;
+                        return {
+                            sessionMin: sessionMin - 1
+                        };
+                    });
+                }
+            }
+        }
+    }, {
+        key: 'sessionIncrease',
+        value: function sessionIncrease() {
+            if (!this.state.running) {
+                if (this.state.sessionMin < 60) {
+                    this.setState(function (_ref2) {
+                        var sessionMin = _ref2.sessionMin;
+                        return {
+                            sessionMin: sessionMin + 1
+                        };
+                    });
+                }
+            }
+        }
+    }, {
+        key: 'breakDecrease',
+        value: function breakDecrease() {
+            if (!this.state.running) {
+                if (this.state.breakMin > 0) {
+                    this.setState(function (_ref3) {
+                        var breakMin = _ref3.breakMin;
+                        return {
+                            breakMin: breakMin - 1
+                        };
+                    });
+                }
+            }
+        }
+    }, {
+        key: 'breakIncrease',
+        value: function breakIncrease() {
+            if (!this.state.running) {
+                if (this.state.breakMin < 60) {
+                    this.setState(function (_ref4) {
+                        var breakMin = _ref4.breakMin;
+                        return {
+                            breakMin: breakMin + 1
+                        };
+                    });
+                }
+            }
+        }
+    }, {
+        key: 'reset',
+        value: function reset() {
+            this.setState({ sessionMin: 25, breakMin: 5, running: false });
+        }
+    }, {
+        key: 'switchState',
+        value: function switchState() {
+            this.setState(function (_ref5) {
+                var running = _ref5.running;
+                return { running: !running };
+            });
+        }
+    }, {
+        key: 'componentWillMount',
+        value: function componentWillMount() {
+            var sessionHolder = this.state.sessionMin,
+                breakHolder = this.state.breakMin;
+            var minute = this.state.sessionMin,
+                second = this.state.secondLeft;
+        }
+    }, {
+        key: 'countSession',
+        value: function countSession() {
+
+            var minute = this.state.sessionMin,
+                second = 0;
+
+            if (this.state.running) {
+                if (second > 0) {
+                    second = second - 1;
+                    this.setState(function (_ref6) {
+                        var secondLeft = _ref6.secondLeft;
+                        return { secondLeft: second };
+                    });
+                }
+
+                if (second === 0) {
+                    if (minute === 0) {
+                        this.countBreak();
+                    } else {
+                        minute = minute - 1;
+                        second = 59;
+                        this.setState(function (_ref7) {
+                            var minuteLeft = _ref7.minuteLeft;
+                            return { minuteLeft: minute, secondLeft: second };
+                        });
+                    }
+                }
+            }
+        }
+    }, {
+        key: 'countBreak',
+        value: function countBreak() {
+            this.setState({ minuteLeft: this.state.breakMin });
+            var minute = this.state.minuteLeft,
+                second = this.state.secondLeft;
+
+            if (second > 0) {
+                this.setState(function (_ref8) {
+                    var second = _ref8.second;
+                    return { second: second - 1 };
+                });
+            }
+
+            if (second === 0) {
+                if (minute === 0) {
+                    this.countSession();
+                } else {
+                    this.setState(function (_ref9) {
+                        var minute = _ref9.minute;
+                        return { minute: minute - 1, second: 59 };
+                    });
+                }
+            }
+        }
+    }, {
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            this.myInterval = setInterval(this.countSession, 1000);
+        }
+    }, {
         key: 'render',
         value: function render() {
             return React.createElement(
                 'div',
                 null,
-                ' HELLO WORLD'
+                React.createElement(
+                    'p',
+                    null,
+                    'POMODORO CLOCK'
+                ),
+                React.createElement(SessionAdjust, { increase: this.sessionIncrease.bind(this), decrease: this.sessionDecrease.bind(this), sessionMin: this.state.sessionMin }),
+                React.createElement(BreakAdjust, { increase: this.breakIncrease.bind(this), decrease: this.breakDecrease.bind(this), breakMin: this.state.breakMin }),
+                React.createElement(Control, { reset: this.reset.bind(this), switchState: this.switchState.bind(this) }),
+                React.createElement(Display, { minuteLeft: this.state.minuteLeft, secondLeft: this.state.secondLeft })
             );
         }
     }]);
@@ -29,4 +186,82 @@ var App = function (_React$Component) {
     return App;
 }(React.Component);
 
+var SessionAdjust = function SessionAdjust(props) {
+
+    return React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'div',
+            { onClick: props.increase },
+            'sessionIncrease'
+        ),
+        React.createElement(
+            'div',
+            null,
+            props.sessionMin
+        ),
+        React.createElement(
+            'div',
+            { onClick: props.decrease },
+            'sessionDecrease'
+        )
+    );
+};
+
+var BreakAdjust = function BreakAdjust(props) {
+
+    return React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'div',
+            { onClick: props.increase },
+            'breakIncrease'
+        ),
+        React.createElement(
+            'div',
+            null,
+            props.breakMin
+        ),
+        React.createElement(
+            'div',
+            { onClick: props.decrease },
+            'breakDecrease'
+        )
+    );
+};
+
+var Control = function Control(props) {
+    return React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'div',
+            { onClick: props.switchState },
+            'Play/Pause'
+        ),
+        React.createElement(
+            'div',
+            { onClick: props.reset },
+            'Reset'
+        )
+    );
+};
+
+var Display = function Display(props) {
+
+    return React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'p',
+            null,
+            'Remaining Time: ',
+            props.minuteLeft,
+            ':',
+            props.secondLeft
+        )
+    );
+};
 ReactDOM.render(React.createElement(App, null), document.querySelector('#App'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,178 @@
 class App extends React.Component{
     constructor(props){
-        super(props);
+       super(props);
+       this.state = {
+           running: false,
+           sessionMin: 25,
+           breakMin: 5,
+           minuteLeft: 25,
+           secondLeft: 0,
+           clockType: 'Session'
+       }
+       this.countSession = this.countSession.bind(this)
+       this.countBreak = this.countBreak.bind(this)
     }
+    
+
+    
+    
+    sessionDecrease(){
+        if (!this.state.running){
+            if(this.state.sessionMin > 0){
+                this.setState(({sessionMin})=>({
+                    sessionMin: sessionMin - 1
+                }))
+            }
+        }
+    }
+    
+    sessionIncrease(){
+        if (!this.state.running){
+            if(this.state.sessionMin < 60){
+                this.setState(({sessionMin})=>({
+                    sessionMin: sessionMin + 1
+                }))
+            }
+        }
+    }
+    
+    breakDecrease(){
+        if (!this.state.running){
+            if(this.state.breakMin > 0){
+                this.setState(({breakMin})=>({
+                    breakMin: breakMin - 1
+                }))
+            }
+        }
+    }
+    
+    breakIncrease(){
+        if (!this.state.running){
+            if(this.state.breakMin < 60){
+                this.setState(({breakMin})=>({
+                    breakMin: breakMin + 1
+                }))
+            }
+        }
+    }
+    
+    reset(){
+        this.setState({sessionMin: 25, breakMin: 5, running: false})
+    }
+    
+    switchState(){
+        this.setState(({running}) => ({running: !running}))
+    }
+    
+    componentWillMount(){
+        const sessionHolder = this.state.sessionMin,
+              breakHolder = this.state.breakMin
+        let minute = this.state.sessionMin,
+            second = this.state.secondLeft
+            
+    }
+    
+    countSession(){
+        
+        let minute = this.state.sessionMin,
+              second = 0
+        
+        if (this.state.running){
+            if (second > 0){
+                second = second - 1
+                this.setState(({secondLeft}) => ({secondLeft: second}))
+            }
+
+            if (second === 0){
+                if (minute === 0){
+                    this.countBreak()
+                } else {
+                    minute = minute - 1
+                    second = 59
+                    this.setState(({minuteLeft}) => ({minuteLeft: minute, secondLeft: second}))
+                }          
+            }
+        }
+    }  
+    
+    countBreak(){
+        this.setState({minuteLeft: this.state.breakMin})
+        const minute = this.state.minuteLeft,
+              second = this.state.secondLeft
+        
+        if (second > 0){
+            this.setState(({second}) => ({second: second - 1}))
+        }
+        
+        if (second === 0){
+            if (minute === 0){
+                this.countSession()
+            } else {
+                this.setState(({minute}) => ({minute: minute - 1, second: 59}))
+            }          
+        }
+    }
+    
+   componentDidMount(){
+      this.myInterval = setInterval(this.countSession,1000)
+   }
+    
     
     render(){
         return(
-        <div> HELLO WORLD</div>
+        <div>
+            <p>POMODORO CLOCK</p>
+            <SessionAdjust increase = {this.sessionIncrease.bind(this)} decrease = {this.sessionDecrease.bind(this)} sessionMin = {this.state.sessionMin} />
+            <BreakAdjust increase = {this.breakIncrease.bind(this)} decrease = {this.breakDecrease.bind(this)} breakMin = {this.state.breakMin} />
+            <Control reset = {this.reset.bind(this)} switchState = {this.switchState.bind(this)}/>
+            <Display minuteLeft = {this.state.minuteLeft} secondLeft = {this.state.secondLeft}/>
+        </div>
         )
     }
 }
     
+let SessionAdjust = (props) => {
+    
+    return (
+        <div>
+            <div onClick = {props.increase}>sessionIncrease</div>
+            <div>{props.sessionMin}</div>
+            <div onClick = {props.decrease}>sessionDecrease</div>
+        </div>
+    )
+}
+
+let BreakAdjust = (props) => {
+    
+    return (
+        <div>
+            <div onClick = {props.increase}>breakIncrease</div>
+            <div>{props.breakMin}</div>
+            <div onClick = {props.decrease}>breakDecrease</div>
+        </div>
+    )
+}
+
+let Control = (props) =>{
+    return(
+        <div> 
+            <div onClick = {props.switchState}>Play/Pause</div>
+            <div onClick = {props.reset}>Reset</div>
+        </div>
+    )
+}
+
+let Display = (props) => {
+   
+    
+    return(
+        <div>
+            
+            <p>Remaining Time: {props.minuteLeft}:{props.secondLeft}</p>
+            
+        </div>
+    
+    )
+    
+}
     ReactDOM.render(<App/>, document.querySelector('#App'))
